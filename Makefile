3c95155dfd7d67712b7c50a37534287a220cca8e
Affan Khamse <66311597+khamseaffan@users.noreply.github.com>
2024-01-30 22:39:12 +0000

    main file updated

--- Makefile
+++ Makefile
@@ -1,22 +1,9 @@
 CXX = g++
 CXXFLAGS = -std=c++11
 
-test_ll: list_tests.h list_todo.h
-	$(CXX) $(CXXFLAGS) $^ -o list_test_run
-	chmod +x list_test_run
-	./list_test_run
-	rm list_test_run
+test_main: test_main.cpp
+	$(CXX) $(CXXFLAGS) $^ -o test_main
 
-test_gll: generic_list_tests.h generic_list_todo.h
-	$(CXX) $(CXXFLAGS) $^ -o gll_test_run
-	chmod +x gll_test_run
-	./gll_test_run
-	rm gll_test_run
-
-test_stack: stack_tests.h stack_todo.h
-	$(CXX) $(CXXFLAGS) $^ -o stack_test_run
-	chmod +x stack_test_run
-	./stack_test_run
-	rm stack_test_run
-
-test: test_ll test_gll test_stack
+test: test_main
+	./test_main
+	rm test_main
--- main.cpp
+++ main.cpp
@@ -4,14 +4,14 @@
 using namespace tests;
 int main() {
   // List Test Cases
-  // run_list_test_case_1();
-  // run_list_test_case_2();
-  // run_list_test_case_3();
-  // run_list_test_case_4();
-  // run_list_test_case_5();
-  // run_list_test_case_6();
-  // run_list_test_case_7();
-  // run_list_test_case_8();
+  run_list_test_case_1();
+  run_list_test_case_2();
+  run_list_test_case_3();
+  run_list_test_case_4();
+  run_list_test_case_5();
+  run_list_test_case_6();
+  run_list_test_case_7();
+  run_list_test_case_8();
 
   // Generic List Test Cases
   run_generic_list_test_case_1();
@@ -24,13 +24,13 @@ int main() {
   run_generic_list_test_case_8();
   
   // Stack Test Cases
-  // run_stack_test_case_1();
-  // run_stack_test_case_2();
-  // run_stack_test_case_3();
-  // run_stack_test_case_4();
-  // run_stack_test_case_5();
-  // run_stack_test_case_6();
-  // run_stack_test_case_7();
-  // run_stack_test_case_8();
+  run_stack_test_case_1();
+  run_stack_test_case_2();
+  run_stack_test_case_3();
+  run_stack_test_case_4();
+  run_stack_test_case_5();
+  run_stack_test_case_6();
+  run_stack_test_case_7();
+  run_stack_test_case_8();
 
 }
