a3b9f2c219b7790e91cf5580123791cd4775908f
Affan Khamse <66311597+khamseaffan@users.noreply.github.com>
2024-01-30 14:35:53 -0500

    Linked_list test code

---
+++ test_linked_list.cpp
@@ -0,0 +1,14 @@
+#include "list_tests.h"
+
+int main() {
+    tests::run_list_test_case_1();
+    tests::run_list_test_case_2();
+    tests::run_list_test_case_3();
+    tests::run_list_test_case_4();
+    tests::run_list_test_case_5();
+    tests::run_list_test_case_6();
+    tests::run_list_test_case_7();
+    tests::run_list_test_case_8();
+
+    return 0;
+}
