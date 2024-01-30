3146b34d7ed662c14be7bec03381cb00322a92d1
Affan Khamse <66311597+khamseaffan@users.noreply.github.com>
2024-01-30 15:26:55 -0500

    Delete run_list_test.sh

--- run_list_test.sh
+++
@@ -1,8 +0,0 @@
-g++ -std=c++11 list_tests.h list_todo.h test_linked_list.cpp -o test_linked_list
-
-if [ $? -eq 0 ]; then
-  ./test_linked_list
-  rm test_linked_list
-else
-  echo "Compilation error"
-fi
