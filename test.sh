6bb8ce4cba2dd2d5ab09f3d1762a03680eca0b64
Affan Khamse <66311597+khamseaffan@users.noreply.github.com>
2024-01-31 00:35:34 +0000

    test script update

--- test.sh
+++ test.sh
@@ -5,31 +5,54 @@ echo
 
 make test_list test_stack test_generic_list
 
-output=$(./test_list)
+# output=$(./test_list)
 
-if [ $? -eq 0 ]; then
-  echo "Pass: List program exited zero"
-else
-  echo "Fail: List program did not exit zero"
+# if [ $? -eq 0 ]; then
+#   echo "Pass: List program exited zero"
+# else
+#   echo "Fail: List program did not exit zero"
+#   exit 1
+# fi
+
+if ./test_list | grep -q 'failed'; then
+  echo "Fail: List program did not exit zero"  
   exit 1
+else
+  echo "Pass: List program exited zero"
+  
 fi
 
 
-output=$(./test_stack)
-if [ $? -eq 0 ]; then
-  echo "Pass: Stack program exited zero"
-else
+# output=$(./test_stack)
+# if [ $? -eq 0 ]; then
+#   echo "Pass: Stack program exited zero"
+# else
+#   echo "Fail: Stack program did not exit zero"
+#   exit 1
+# fi
+ 
+if ./test_stack | grep -q 'failed'; then
   echo "Fail: Stack program did not exit zero"
   exit 1
+else
+  echo "Pass: Stack program exited zero"
 fi
 
-output=$(./test_generic_list)
-if [ $? -eq 0 ]; then
-  echo "Pass: Generic list program exited zero"
-else
-  echo "Fail: Generic list program did not exit zero"
+# output=$(./test_generic_list)
+# if [ $? -eq 0 ]; then
+#   echo "Pass: Generic list program exited zero"
+# else
+#   echo "Fail: Generic list program did not exit zero"
+#   exit 1
+# fi
+
+if ./test_generic_list | grep -q 'failed'; then
+  echo "Fail: Generic program did not exit zero"
   exit 1
+else
+  echo "Pass: Generic program exited zero"
 fi
+
 
 echo
 echo "All tests passed."
